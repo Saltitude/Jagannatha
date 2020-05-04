effab956b18bbd483a4c08f13833d9ed0d840b97
Zainix14 <49592220+Zainix14@users.noreply.github.com>
2020-05-04 17:38:09 +0200

    push push le ptit push du line renderer ecran droite

---
+++ Assets/_GameAssets/OperatorSoftware/_Prefabs/SC_UI_BlinkBreakdownManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_UI_BlinkBreakdownManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static SC_UI_BlinkBreakdownManager _instance;
+    public static SC_UI_BlinkBreakdownManager Instance { get { return _instance; } }
+
+    #endregion
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
