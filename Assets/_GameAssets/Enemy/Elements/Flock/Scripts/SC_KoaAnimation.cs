5c6248e79e44bf634fea4b2d13439d71b2dfcc02
Zainix14 <49592220+Zainix14@users.noreply.github.com>
2020-05-08 17:37:00 +0200

    push push preparation anim

---
+++ Assets/_GameAssets/Enemy/Elements/Flock/Scripts/SC_KoaAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_KoaAnimation : MonoBehaviour
+{
+
+    [SerializeField]
+    int color;
+
+    
+    // Start is called before the first frame update
+
+    Renderer[] renderer;
+    void Start()
+    {
+        renderer = GetComponentsInChildren<Renderer>();
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(color);
+        for (int i = 0; i < renderer.Length; i++)
+        {
+            //renderer[i].material.SetColor("_EmissionColor", color);
+
+        }
+    }
+}
