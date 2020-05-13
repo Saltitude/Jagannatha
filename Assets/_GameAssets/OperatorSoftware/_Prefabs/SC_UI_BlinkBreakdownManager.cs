03f3a068ab1d48e9bb02da0a7f505bc60dd7a014
Zainix14 <49592220+Zainix14@users.noreply.github.com>
2020-05-13 12:14:22 +0200

    Vision Cone

--- Assets/_GameAssets/OperatorSoftware/_Prefabs/SC_UI_BlinkBreakdownManager.cs
+++ Assets/_GameAssets/OperatorSoftware/_Prefabs/SC_UI_BlinkBreakdownManager.cs
@@ -13,8 +13,12 @@ public class SC_UI_BlinkBreakdownManager : MonoBehaviour
     #endregion
 
     bool breakdown = false;
+
+    [SerializeField]
+    public Image vision;
     private void Awake()
     {
+
         if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
@@ -40,6 +44,7 @@ public class SC_UI_BlinkBreakdownManager : MonoBehaviour
     {
         toAnimate = new List<Image>();
         All = FindObjectsOfType<Image>();
+        vision = GameObject.FindGameObjectWithTag("VisionCone").GetComponent<Image>();
         foreach(Image b in All)
         {
             if (b.material == matToBlink)
@@ -54,12 +59,17 @@ public class SC_UI_BlinkBreakdownManager : MonoBehaviour
     void Update()
     {
         if(breakdown)
-        for (int i = 0; i < toAnimate.Count; i++)
         {
             color = new Color(color.r, color.g, color.b, opacity);
-            toAnimate[i].color = color;
 
+            for (int i = 0; i < toAnimate.Count; i++)
+            {
+                toAnimate[i].color = color;
+
+            }
+            vision.color = new Color(0, 0,0, 0.75f-(opacity*0.7f));
         }
+
 
     }
 
@@ -68,12 +78,35 @@ public class SC_UI_BlinkBreakdownManager : MonoBehaviour
         breakdown = b;
         if(b == false)
         {
+            StartCoroutine(LerpOpacity());
+        }
+    }
+
+    IEnumerator LerpOpacity()
+    {
+        while(color.a <0.99)
+        {
+            float curOpacity = Mathf.Lerp(color.a, 1f, 0.10f); 
             for (int i = 0; i < toAnimate.Count; i++)
             {
-                color = new Color(color.r, color.g, color.b, 1);
+                color = new Color(color.r, color.g, color.b, curOpacity);
                 toAnimate[i].color = color;
+
             }
+            Color colorVision = new Color(0, 0, 0, 0.75f - (curOpacity *0.7f));
+            vision.color = colorVision;
+
+            yield return 0;
         }
+        for (int i = 0; i < toAnimate.Count; i++)
+        {
+            color = new Color(color.r, color.g, color.b, 1);
+            toAnimate[i].color = color;
+
+        }
+        vision.color = new Color(0, 0, 0,  0);
+        StopAllCoroutines();
+
     }
 
 }
