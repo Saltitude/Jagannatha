4912e0fb01c1f995ab569e51b78f329ec7e95a86
Peritios <49589640+Peritios@users.noreply.github.com>
2020-05-19 14:45:09 +0200

    des tests

---
+++ Assets/_GameAssets/Mecha/Interactions_Cockpit/BreakDownMangement/Weapon_BreakDown/SC_ShowTourbiLvl_OP.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_ShowTourbiLvl_OP : MonoBehaviour
+{
+
+    [Header("References")]
+    [SerializeField]
+    RectTransform[] tab_TorbiBar;
+
+    [SerializeField]
+    float f_MaxLenght;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateBar();
+    }
+
+    void UpdateBar()
+    {
+        for(int i = 0; i < tab_TorbiBar.Length; i++)
+        {
+
+            float TargetValue = SC_SyncVar_BreakdownWeapon.Instance.SL_Tourbilols[i].valueWanted;
+            float CurrentValue = SC_SyncVar_BreakdownWeapon.Instance.SL_Tourbilols[i].value;
+
+            if (CurrentValue < 0)
+                CurrentValue *= -1;
+
+            if(TargetValue == 1)
+                tab_TorbiBar[i].rotation = Quaternion.Euler(0, 0, 180);
+            else
+                tab_TorbiBar[i].rotation = Quaternion.Euler(0, 0, 0);
+
+            tab_TorbiBar[i].SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, CurrentValue * f_MaxLenght);
+
+        }
+    }
+
+}
