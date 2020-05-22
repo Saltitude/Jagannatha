c680fe2c1253289df1974d83d5e6fcbde8bbe914
Peritios <49589640+Peritios@users.noreply.github.com>
2020-05-22 14:49:36 +0200

    add a Tab Orientation in Wave Settings

--- Assets/_GameAssets/Enemy/Elements/Phase/PhaseSettings.cs
+++ Assets/_GameAssets/Enemy/Elements/Phase/PhaseSettings.cs
@@ -9,5 +9,8 @@ public class PhaseSettings : ScriptableObject
     [Header("Waves")]
     public WaveSettings[] waves;
 
+    public enum Orientation { NorthDefault, East, South, West }
+    public Orientation[] WavesOrientation;
+
 
 }
