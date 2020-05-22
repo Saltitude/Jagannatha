66b0ddf360b5bec0faf51a3be0ce80abf50a2aae
Peritios <49589640+Peritios@users.noreply.github.com>
2020-05-22 14:59:37 +0200

    change order

--- Assets/_GameAssets/Enemy/Elements/Phase/PhaseSettings.cs
+++ Assets/_GameAssets/Enemy/Elements/Phase/PhaseSettings.cs
@@ -9,7 +9,7 @@ public class PhaseSettings : ScriptableObject
     [Header("Waves")]
     public WaveSettings[] waves;
 
-    public enum Orientation { NorthDefault, East, South, West }
+    public enum Orientation { NorthDefault, West, South, East }
     public Orientation[] WavesOrientation;
 
 
