1bdc2f51c955e4cb29ef70b869534b2cd3cd5f47
Peritios <49589640+Peritios@users.noreply.github.com>
2020-05-28 15:22:02 +0200

    des baux pour le changement de scene

---
+++ Assets/_GameAssets/System/Network/Scripts/SC_NetScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SC_NetScene : NetworkBehaviour
+{
+    [Command]
+    public void CmdSendReadyOP()
+    {
+        //SC_SceneManager.Instance.b_OperatorReadyToLoad = true;
+    }
+}
--- Assets/_GameAssets/System/Scenes/SC_SceneManager.cs
+++ Assets/_GameAssets/System/Scenes/SC_SceneManager.cs
@@ -6,20 +6,53 @@ using UnityEngine.SceneManagement;
 
 public class SC_SceneManager : NetworkBehaviour
 {
+    /*
+    #region Singleton
+
+    private static SC_SceneManager _instance;
+    public static SC_SceneManager Instance { get { return _instance; } }
+
+    #endregion*/
 
     GameObject Mng_CheckList = null;
 
     [SyncVar]
     public int n_ConnectionsCount = 0;
-
     float countTime = 0;
+
+    [SerializeField, Range(0, 1)]
+    float f_LoadingProgress;
+    [SyncVar]
+    public bool b_PilotReadyToLoad = false;
+    [SyncVar]
+    public bool b_OperatorReadyToLoad = false;
+    [SyncVar]
+    public bool b_LoadingAllowed = false;
 
     [SerializeField]
     SC_passwordLock _SC_PasswordLock;
-    // Start is called before the first frame update
+
+    /*
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }*/
+
     void Start()
     {
+
         IsCheck();
+
+        if (SceneManager.GetActiveScene().buildIndex == 2 || SceneManager.GetActiveScene().buildIndex == 1)
+            StartCoroutine(PreLoadScene());
+
     }
 
     void IsCheck()
@@ -31,8 +64,13 @@ public class SC_SceneManager : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (SceneManager.GetActiveScene().buildIndex == 2 || SceneManager.GetActiveScene().buildIndex == 1)
             LobbyUpdate();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isServer)
+            RpcAllowChangeScene();
+
     }
 
     void LobbyUpdate()
@@ -44,66 +82,71 @@ public class SC_SceneManager : NetworkBehaviour
         //si les deux joueurs sont connectés
         if (n_ConnectionsCount >= 2)
         {
-
             //si pas Server on load la scène opérateur
-            if (!isServer)
-            {
-                //_SC_PasswordLock.b_IsConnected = true;
-                LoadTutoOperator();
-
-                if (_SC_PasswordLock != null)
-                {
-                    _SC_PasswordLock.validatePassword();
-                        
-                }
-            }
-                
-
-            //si server on invoke le chargement de la scène pilote (la scène opérateur necessitant de se lancer en première)
-            //sécurisé par b_hasInvoked pour n'être appelé qu'une fois
-            if (isServer)
-            {
-                LoadTutoPilot();
-            }
-
+            if (!isServer && _SC_PasswordLock != null)          
+                _SC_PasswordLock.validatePassword();                                   
         }
 
     }
 
-    void LoadTutoLobby()
-    {
-        SceneManager.LoadScene(1);
-    }
-    void LoadTutoLobbyOpe()
-    {
-        SceneManager.LoadScene(2);
-        //SC_NetPlayerInit_OP.Instance.CmdSendForceUpdate();
-        Mng_CheckList.GetComponent<SC_CheckList>().NetworkPlayerOperator.GetComponent<SC_NetPlayerInit_OP>().CmdSendForceUpdate();
-
-
-    }
-
-    void LoadTutoPilot()
+    IEnumerator PreLoadScene()
     {
 
-        SceneManager.LoadScene(3);
-       
+        yield return null;
 
+        Debug.Log("PreLoadScene ");
+
+        AsyncOperation asyncOperation = null;
+
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+            asyncOperation = SceneManager.LoadSceneAsync(3);
+
+        else if (SceneManager.GetActiveScene().buildIndex == 2)
+            asyncOperation = SceneManager.LoadSceneAsync(3);
+
+        asyncOperation.allowSceneActivation = false;
+
+        f_LoadingProgress = asyncOperation.progress;
+
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+
+            Debug.Log("notDone");
+
+            //Output the current progress (In Inspector)
+            f_LoadingProgress = asyncOperation.progress;
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f && n_ConnectionsCount >= 2)
+            {
+
+                if (SceneManager.GetActiveScene().buildIndex == 1 && !b_PilotReadyToLoad)
+                    b_PilotReadyToLoad = true;
+
+                else if (SceneManager.GetActiveScene().buildIndex == 2 && !b_OperatorReadyToLoad)
+                    SendReadyOP();
+
+                //Activate the Scene
+                if (b_LoadingAllowed && b_PilotReadyToLoad && b_OperatorReadyToLoad)                    
+                    asyncOperation.allowSceneActivation = true;
+
+            }
+
+            yield return null;
+
+        }
     }
 
-    void LoadTutoOperator()
+    void SendReadyOP()
     {
-        SceneManager.LoadScene(4);
+        Mng_CheckList.GetComponent<SC_CheckList>().NetworkPlayerOperator.GetComponent<SC_NetScene>().CmdSendReadyOP();
     }
 
-    //void LoadGamePilot()
-    //{
-    //    SceneManager.LoadScene(4);
-    //}
-
-    //void LoadGameOperator()
-    //{
-    //    SceneManager.LoadScene(5);
-    //}
+    [ClientRpc]
+    void RpcAllowChangeScene()
+    {
+        b_LoadingAllowed = true;
+    }
 
 }
